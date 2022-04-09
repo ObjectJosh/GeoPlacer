405528ea5999e4122d00c0e014abd81169135635
Josh Wong <joshwong74@gmail.com>
2022-04-09 10:13:02 -0700

    Add heroku config

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+
+require('./server/database');
+const squares = require('./server/routes/squares');
+app.use('/squares', squares);
+
+const PORT  = process.env.PORT || 8080;
+
+app.get('', (req, res) => {
+    res.send('<h1>Our App</h1>');
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
