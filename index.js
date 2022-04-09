b2bcf0313755400613fb5d98ccf1853cf83a83bf
Josh Wong <joshwong74@gmail.com>
2022-04-09 15:23:42 -0700

    Working on favicon rendering

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+var favicon = require('serve-favicon');
+
 const app = express();
 
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 require('./server/database');
 const squares = require('./server/routes/squares');
