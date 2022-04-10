cda64007d0558b3df60baac5721dbf22c2f9a5f4
Josh Wong <joshwong74@gmail.com>
2022-04-09 19:26:20 -0700

    Chat to App.js

--- client/App.js
+++ client/App.js
@@ -9,15 +9,17 @@ import {
 } from '@mui/material';
 
 import Map from './components/Map';
+import User from './components/User';
 import { getSquares } from './api/square';
 import Colors from './components/Colors';
 import earthImage from './img/earth.png';
 
 
 function App() {
-    const [username, setUsername] = useState("");
-    const [room, setRoom] = useState("");
     const [showChat, setShowChat] = useState(false);
+    // const [username, setUsername] = useState("");
+    // const [room, setRoom] = useState("");
+    // const [showChat, setShowChat] = useState(false);
 
     const [squares, setSquares] = useState([]);
     const colors = [
@@ -64,50 +66,64 @@ function App() {
         }
     }
 
+    const renderLogin = () => {
+        return (
+            <User showChat={showChat} setShowChat={setShowChat}/>
+        )
+    }
+
+    const renderBody = () => {
+        return (
+            <Box sx={{ position: 'relative' }}>
+                <Link onClick={executeScroll} sx={{ '&:hover:': { cursor: 'pointer' }, mt: '2rem' }}>
+                    <Typography variant='h5' sx={{ mb: '2rem' }}>
+                        How to play
+                    </Typography>
+                </Link>
+                <Map squares={squares} selectedColor={colors[colorIndex]} handleGetSquares={handleGetSquares} />
+                <Colors colors={colors} colorIndex={colorIndex} setColorIndex={setColorIndex} />
+                <Box sx={{ position: 'relative', mt: '8rem' }}>
+                    <img alt='world' src={earthImage} style={{ position: 'absolute', width: '80%', height: 'auto', transform: 'translate(-50%, -20%)', opacity: 0.8, zIndex: -5 }} />
+                    <Typography variant='h4' ref={descriptionRef} sx={{ mb: '1rem', fontWeight: '700' }}>
+                        How to play
+                    </Typography>
+                    <Typography variant='body1' sx={{ fontSize: '1.4rem', mb: '2rem', fontWeight: '500' }}>
+                        Walk around and color in squares. It's that simple!
+                    </Typography>
+                    <Typography variant='body1' sx={{ fontSize: '1.5rem', transform: 'rotate(-15deg) translateX(-8rem)', mb: '2rem', color: '#fc9803', fontWeight: '600' }}>
+                        Paint a picture with friends!
+                    </Typography>
+                    <Typography variant='body1' sx={{ fontSize: '1.5rem', transform: 'rotate(15deg) translateX(8rem)', mb: '4rem', color: '#00de3b', fontWeight: '600' }}>
+                        Collaborate with others!
+                    </Typography>
+                    <Typography variant='body1' sx={{ fontSize: '1.5rem', transform: 'rotate(-10deg) translateX(-3rem)', mb: '2rem', color: '#ff3b29', fontWeight: '600' }}>
+                        Just have some fun!
+                    </Typography>
+                </Box>
+                <Box sx={{ mt: '15rem' }}></Box>
+                <Typography variant='body2' sx={{ mt: '10rem', fontSize: '1rem', fontWeight: '600' }}>
+                    Built for your enjoyment by
+                </Typography>
+                <Typography variant='body2' sx={{ mt: '0.5rem' }}>
+                    Josh Wong, Christian Honenin, Wesley Luu & Wilson Szeto
+                </Typography>
+                <Typography variant='body2' sx={{ mt: '2rem', fontSize: '1rem', fontWeight: '600' }}>
+                    Inspired by
+                </Typography>
+                <Link onClick={() => window.open('https://www.reddit.com/r/place/')}>
+                    <Typography variant='body2' sx={{ mt: '0.5rem' }} >
+                        r/Place
+                    </Typography>
+                </Link>
+                {renderLogin()}
+            </Box>
+        );
+    }
+
     return (
         <Container sx={styles.mainContainer}>
             <Typography variant='h2' sx={styles.header}>Welcome to GeoPlacer</Typography>
-            <Colors colors={colors} colorIndex={colorIndex} setColorIndex={setColorIndex} />
-            <Link onClick={executeScroll} sx={{ '&:hover:': { cursor: 'pointer' }, mt: '2rem' }}>
-                <Typography variant='h5' sx={{ mb: '2rem' }}>
-                    How to play
-                </Typography>
-            </Link>
-            <Map squares={squares} selectedColor={colors[colorIndex]} handleGetSquares={handleGetSquares} />
-            <Colors colors={colors} colorIndex={colorIndex} setColorIndex={setColorIndex} />
-            <Box sx={{ position: 'relative', mt: '8rem' }}>
-                <img alt='world' src={earthImage} style={{ position: 'absolute', width: '80%', height: 'auto', transform: 'translate(-50%, -20%)', opacity: 0.8, zIndex: -5 }} />
-                <Typography variant='h4' ref={descriptionRef} sx={{ mb: '1rem', fontWeight: '700' }}>
-                    How to play
-                </Typography>
-                <Typography variant='body1' sx={{ fontSize: '1.4rem', mb: '2rem', fontWeight: '500' }}>
-                    Walk around and color in squares. It's that simple!
-                </Typography>
-                <Typography variant='body1' sx={{ fontSize: '1.5rem', transform: 'rotate(-15deg) translateX(-8rem)', mb: '2rem', color: '#fc9803', fontWeight: '600' }}>
-                    Paint a picture with friends!
-                </Typography>
-                <Typography variant='body1' sx={{ fontSize: '1.5rem', transform: 'rotate(15deg) translateX(8rem)', mb: '4rem', color: '#00de3b', fontWeight: '600' }}>
-                    Collaborate with others!
-                </Typography>
-                <Typography variant='body1' sx={{ fontSize: '1.5rem', transform: 'rotate(-10deg) translateX(-3rem)', mb: '2rem', color: '#ff3b29', fontWeight: '600' }}>
-                    Just have some fun!
-                </Typography>
-            </Box>
-            <Box sx={{ mt: '15rem' }}></Box>
-            <Typography variant='body2' sx={{ mt: '10rem', fontSize: '1rem', fontWeight: '600' }}>
-                Built for your enjoyment by
-            </Typography>
-            <Typography variant='body2' sx={{ mt: '0.5rem' }}>
-                Josh Wong, Christian Honenin, Wesley Luu & Wilson Szeto
-            </Typography>
-            <Typography variant='body2' sx={{ mt: '2rem', fontSize: '1rem', fontWeight: '600' }}>
-                Inspired by
-            </Typography>
-            <Link onClick={() => window.open('https://www.reddit.com/r/place/')}>
-                <Typography variant='body2' sx={{ mt: '0.5rem' }} >
-                    r/Place
-                </Typography>
-            </Link>
+            {showChat ? renderBody() : renderLogin()}
         </Container>
     );
 };
