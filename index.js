ba853d6cdb49b7d59a6f1b2cddc02ff5c14f1e49
Westluu <wluu03@calpoly.edu>
2022-04-10 02:19:51 -0700

    trying port 8080

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
 
-const CHATPORT = process.env.CHATPORT || 3001;
+const CHATPORT = process.env.CHATPORT || 8080;
 
 server.listen(CHATPORT, () => {
   console.log(`Chat server running on port ${CHATPORT}`);
