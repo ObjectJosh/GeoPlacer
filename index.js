66b21c43c5a7af90069dff0dd50efbce5d637135
Christian Honein <chonein@outlook.com>
2022-04-09 20:58:44 -0700

    redirect http to https

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 var favicon = require('serve-favicon');
 
 const app = express();
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
 /* Sockets */
 const http = require("http");
 const cors = require("cors");
@@ -21,43 +29,43 @@ app.use('/squares', squares);
 
 /* Sockets */
 const io = new Server(server, {
-    cors: {
-      origin: "http://localhost:8080",
-      methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST"],
+  },
 });
 io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
-  
-    socket.on("join_chat", (data) => {
-      socket.join(data);
-      console.log(`User with ID: ${socket.id} joined room: ${data}`);
-      count++
-      console.log("count: " + count)
-    });
-  
-    socket.on("send_message", (data) => {
-      socket.to(data.room).emit("receive_message", data);
-    });
-  
-    socket.on("disconnect", () => {
-      console.log("User Disconnected", socket.id);
-      count--;
-    });
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_chat", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    count++
+    console.log("count: " + count)
   });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+    count--;
+  });
+});
 
 const PORT = process.env.PORT || 8080;
 
 app.get('', (req, res) => {
-    res.send('<h1>Our App</h1>');
+  res.send('<h1>Our App</h1>');
 });
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}`);
+  console.log(`App running on port ${PORT}`);
 });
 
 const CHATPORT = process.env.CHATPORT || 3001;
 
 server.listen(CHATPORT, () => {
-    console.log(`Chat server running on port ${CHATPORT}`);
+  console.log(`Chat server running on port ${CHATPORT}`);
 });
